c07e07e7519ca5122c793c1b46f18366e80e68ae
Vlad <2000californiadream2000@gmail.com>
2021-05-18 23:43:03 +0300

    Server replies

---
+++ src/server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const port = 3000;
+
+export default async () => {
+    const app = express();
+
+    app.use(express.static(process.cwd() + "/src/server/public"));
+
+    app.listen( process.env.PORT || port, () => {
+        console.log(`Server is running at port ${port}`)
+    })
+}
